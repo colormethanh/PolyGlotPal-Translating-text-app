102cafa1c529cf404224781537e29e9cde595401
Colormethanh <thnguyen1847@gmail.com>
2024-12-05 15:22:21 -0500

    added axios

--- PolyGlotPal/src/App.jsx
+++ PolyGlotPal/src/App.jsx
@@ -1,67 +1,81 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-import MessagesContainer from './components/messagesContainer';
-import InputContainer from './components/inputContainer';
-import NavBar from './components/navBar';
+import MessagesContainer from "./components/messagesContainer";
+import InputContainer from "./components/inputContainer";
+import NavBar from "./components/navBar";
 
 const languageCodes = {
-  'ja': 'Japanese',
-  'en': 'English',
-  'es': 'Spanish',
-  'vi': 'vietnamese' 
-}
+  ja: "Japanese",
+  en: "English",
+  es: "Spanish",
+  vi: "vietnamese",
+};
 
 function App() {
-  const [languageSettings, setLanguageSettings] = useState({inputLang: 'en', outputLang: 'ja'})
-  const [messages, setMessages] = useState([{type: 'received', text:`Hello There, type something and I will do my best to translate it.`}]);
-  
-  function addMessage(text, type='sent') {
+  const [languageSettings, setLanguageSettings] = useState({
+    inputLang: "en",
+    outputLang: "ja",
+  });
+  const [messages, setMessages] = useState([
+    {
+      type: "received",
+      text: `Hello There, type something and I will do my best to translate it.`,
+    },
+  ]);
+
+  function addMessage(text, type = "sent") {
     // add our own message
-    let message = {type:type, text: text}
-    
+    let message = { type: type, text: text };
+
     setMessages((prevMessages) => {
-      return [...prevMessages, message]
-    })
+      return [...prevMessages, message];
+    });
   }
 
   async function translateMessage(text) {
     // API link
     // https://rapidapi.com/translated/api/mymemory-translation-memory/
     const options = {
-      method: 'GET',
-      url: 'https://translated-mymemory---translation-memory.p.rapidapi.com/get',
+      method: "GET",
+      url: "https://translated-mymemory---translation-memory.p.rapidapi.com/get",
       params: {
         langpair: `${languageSettings.inputLang}|${languageSettings.outputLang}`,
         q: text,
-        mt: '1',
-        onlyprivate: '0',
-        de: 'a@b.c'
+        mt: "1",
+        onlyprivate: "0",
+        de: "a@b.c",
       },
       headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
-        'X-RapidAPI-Host': import.meta.env.VITE_API_HOST
-      }
+        "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+        "X-RapidAPI-Host": import.meta.env.VITE_API_HOST,
+      },
     };
     try {
       const response = await axios.request(options);
       console.log(response.data.responseData.translatedText);
-      addMessage(response.data.responseData.translatedText,'received');
+      addMessage(response.data.responseData.translatedText, "received");
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <div id='app-container'>
-      <NavBar languageSettings={languageSettings} setLanguageSettings={setLanguageSettings}/>
+    <div id="app-container">
+      <NavBar
+        languageSettings={languageSettings}
+        setLanguageSettings={setLanguageSettings}
+      />
       <div id="chat-box">
         <MessagesContainer messages={messages} />
-        <InputContainer addMessage={addMessage} translateMessage={translateMessage}/>
+        <InputContainer
+          addMessage={addMessage}
+          translateMessage={translateMessage}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
